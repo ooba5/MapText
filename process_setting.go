b7285bddd9174a13aaf99ca2218d42f0fe7eb45d
ooba5 <cwheffer@gmail.com>
2023-01-22 15:11:09 -0500

    adding main

--- process_setting.go
+++ process_setting.go
@@ -4,11 +4,17 @@ import(
 )
 func main(){
 
-	text, err := ReadBook("path")
+	// text, err := ReadBook("path")
 
+	// if err != nil{
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(text)
+
+	food, err := ReadFoods("./Data/Food/food.csv")
 	if err != nil{
 		fmt.Println(err)
 	}
-	fmt.Println(text)
+	fmt.Println(food)
 
 }
